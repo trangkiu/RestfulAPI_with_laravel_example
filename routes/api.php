65655b3a52165ddb17f88a33629adcf358760a1d
trangng <trang.minhnguyen2610@gmail.com>
2020-01-24 11:36:16 -0500

    add controller and api route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// this resource command will create REST control include create and edit
+// however because we are using API so we dont have any page to show create and edit
+// so we will u apiResource instead
+// Route::resource('/products','ProductController');
+Route::apiResource('/products','ProductController');
+
+// when it comes to review, the url will looks like products/{productId}/reviews
+// so we need to group them to give prefix
+Route::group(['prefix'=>'products'],function(){
+  Route::apiResource('/{product}/reviews', 'ReviewController');
+});
